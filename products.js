34e3e6088d3082b2349fdc75e4a4c89782970779
silvanosa <felipelojabaratao@gmail.com>
2026-03-12 11:45:22 -0300

    novo produto

--- products.js
+++ products.js
@@ -1,1 +1,18 @@
+const products = [];
 const products = [
+
+{
+name: "Produto teste",
+price: "R$100",
+image: "imagem.jpg",
+link: "#"
+},
+
+{
+name: "Máquina de Barbear",
+price: "R$89",
+image: "link-da-imagem",
+link: "link-afiliado"
+}
+
+];
